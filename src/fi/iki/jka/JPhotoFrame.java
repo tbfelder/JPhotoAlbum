a2fc46cad7d9e02dbb946eb9b1fdca49e0308a58
Ivan Moore <ivan@teamoptimization.com>
2019-03-20 13:51:14 +0000

    mock SlideShowViewer

--- src/fi/iki/jka/JPhotoFrame.java
+++ src/fi/iki/jka/JPhotoFrame.java
@@ -80,6 +80,8 @@ public class JPhotoFrame extends JFrame
     public static String PAGEINFO = "pageinfo";
     public static String PHOTO_DIR = "photo_directory";
 
+    SlideShowViewer slideShowViewer = new JSlideShowViewer();
+
     protected Preferences prefs = null;
     protected String albumFileName = null;
     protected JPhotoList list = null;
@@ -546,7 +548,7 @@ public class JPhotoFrame extends JFrame
         else if (cmd.equals(JPhotoMenu.A_WATERMARK)) {
             String def = photos.getWatermark();
             if (def.equals(""))
-                def = "© "+Calendar.getInstance().get(Calendar.YEAR)+" ";
+                def = "ï¿½ "+Calendar.getInstance().get(Calendar.YEAR)+" ";
             String res = JOptionPane.showInputDialog(this, "Watermark",
                                                      def);
             if (res!=null)
@@ -580,14 +582,7 @@ public class JPhotoFrame extends JFrame
             showExif();
         }
         else if (cmd.equals(JPhotoMenu.A_SLIDESHOW)) {
-            if (photos.getSize()>0) {
-                JPhotoShow show = new JPhotoShow(photos, 5000, list);
-                show.setVisible(true);
-            }
-            else
-                JOptionPane.showMessageDialog(this, "No photos to show!",
-                                              APP_NAME, JOptionPane.ERROR_MESSAGE);
-                
+            slideShowViewer.openSlideShow(5000);
         }
         else if (cmd.equals(JPhotoMenu.A_HELP)) {
             displayHelp();
@@ -596,7 +591,7 @@ public class JPhotoFrame extends JFrame
             JOptionPane.showMessageDialog(this, APP_NAME+" v1.4.5 - Organize and Publish Your Digital Photos.\n"+
                                           "Copyright 2005-2007 Jari Karjala [www.jpkware.com],\n"
                                           +"Tarja Hakala [www.hakalat.net]"
-                                          +" and Zbynek Mužík [zbynek.muzik@email.cz]\n"
+                                          +" and Zbynek Muï¿½ï¿½k [zbynek.muzik@email.cz]\n"
                                           +"This is free software, licenced under the GNU General Public License.",
                                           JPhotoMenu.A_ABOUT, JOptionPane.INFORMATION_MESSAGE);
         }
@@ -624,6 +619,18 @@ public class JPhotoFrame extends JFrame
             System.out.println("Not implemented: "+cmd);
         
         setTitle();
+    }
+
+    class JSlideShowViewer implements SlideShowViewer {
+        public void openSlideShow(int interval) {
+            if (photos.getSize()>0) {
+                JPhotoShow show = new JPhotoShow(photos, interval, list);
+                show.setVisible(true);
+            }
+            else
+                JOptionPane.showMessageDialog(JPhotoFrame.this, "No photos to show!",
+                        APP_NAME, JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     public void insertPhotos(String files[]) {
---
+++ src/fi/iki/jka/SlideShowViewer.java
@@ -0,0 +1,5 @@
+package fi.iki.jka;
+
+interface SlideShowViewer {
+    void openSlideShow(int interval);
+}
