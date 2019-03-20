f145f36997b7830a9f4e7905d288cafe90e5f7c5
Ivan Moore <ivan@teamoptimization.com>
2019-03-20 13:52:47 +0000

    not-recommended-using-field

--- src/fi/iki/jka/JPhotoFrame.java
+++ src/fi/iki/jka/JPhotoFrame.java
@@ -99,7 +99,8 @@ public class JPhotoFrame extends JFrame
     protected File photoDirectory = null;
     
     protected static HashMap allFrames = new HashMap();
-    
+    int interval;
+
     protected JPhotoFrame() throws Exception {
         // Do nothing... needed for inheritance !
     }
@@ -546,7 +547,7 @@ public class JPhotoFrame extends JFrame
         else if (cmd.equals(JPhotoMenu.A_WATERMARK)) {
             String def = photos.getWatermark();
             if (def.equals(""))
-                def = "© "+Calendar.getInstance().get(Calendar.YEAR)+" ";
+                def = "ï¿½ "+Calendar.getInstance().get(Calendar.YEAR)+" ";
             String res = JOptionPane.showInputDialog(this, "Watermark",
                                                      def);
             if (res!=null)
@@ -580,14 +581,8 @@ public class JPhotoFrame extends JFrame
             showExif();
         }
         else if (cmd.equals(JPhotoMenu.A_SLIDESHOW)) {
-            if (photos.getSize()>0) {
-                JPhotoShow show = new JPhotoShow(photos, 5000, list);
-                show.setVisible(true);
-            }
-            else
-                JOptionPane.showMessageDialog(this, "No photos to show!",
-                                              APP_NAME, JOptionPane.ERROR_MESSAGE);
-                
+            interval = 5000;
+            openSlideShow();
         }
         else if (cmd.equals(JPhotoMenu.A_HELP)) {
             displayHelp();
@@ -596,7 +591,7 @@ public class JPhotoFrame extends JFrame
             JOptionPane.showMessageDialog(this, APP_NAME+" v1.4.5 - Organize and Publish Your Digital Photos.\n"+
                                           "Copyright 2005-2007 Jari Karjala [www.jpkware.com],\n"
                                           +"Tarja Hakala [www.hakalat.net]"
-                                          +" and Zbynek Mužík [zbynek.muzik@email.cz]\n"
+                                          +" and Zbynek Muï¿½ï¿½k [zbynek.muzik@email.cz]\n"
                                           +"This is free software, licenced under the GNU General Public License.",
                                           JPhotoMenu.A_ABOUT, JOptionPane.INFORMATION_MESSAGE);
         }
@@ -624,6 +619,16 @@ public class JPhotoFrame extends JFrame
             System.out.println("Not implemented: "+cmd);
         
         setTitle();
+    }
+
+    void openSlideShow() {
+        if (photos.getSize()>0) {
+            JPhotoShow show = new JPhotoShow(photos, interval, list);
+            show.setVisible(true);
+        }
+        else
+            JOptionPane.showMessageDialog(this, "No photos to show!",
+                                          APP_NAME, JOptionPane.ERROR_MESSAGE);
     }
 
     public void insertPhotos(String files[]) {
--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -2,12 +2,30 @@ package fi.iki.jka;
 
 import org.junit.Test;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class JPhotoFrameTest {
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void showsSlidesWithStandardInterval() throws Exception {
+        JPhotoFrame photoFrame = new JPhotoFrame() {
+            @Override
+            void openSlideShow() {
+            }
+
+            @Override
+            public void setTitle() {
+            }
+        };
+
+        photoFrame.actionPerformed(new ActionEvent(this, 0, JPhotoMenu.A_SLIDESHOW));
+
+        assertThat(photoFrame.interval, equalTo(5000));
     }
 }
