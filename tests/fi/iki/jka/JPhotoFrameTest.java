5865bf72829e8d6c5969454907907eb312b786a9
Ivan Moore <ivan@teamoptimization.com>
2019-03-20 13:49:39 +0000

    subclassing-JPhotoFrame

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -2,12 +2,33 @@ package fi.iki.jka;
 
 import org.junit.Test;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class JPhotoFrameTest {
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void showsSlidesWithStandardInterval() throws Exception {
+        final List<Integer> slideShowIntervals = new ArrayList<Integer>();
+
+        JPhotoFrame photoFrame = new JPhotoFrame() {
+            @Override
+            void openSlideShow(int interval) {
+                slideShowIntervals.add(interval);
+            }
+
+            @Override
+            public void setTitle() {
+            }
+        };
+
+        photoFrame.actionPerformed(new ActionEvent(this, 0, JPhotoMenu.A_SLIDESHOW));
+
+        assertThat(slideShowIntervals, equalTo(singletonList(5000)));
     }
 }
